700353b5e04f26d35894874982b5539eef6e991d
unknown <coopertrooper7@gmail.com>
2023-02-18 12:33:33 -0800

    Initial supabase auth

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,32 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { supabase } from './supabaseClient'
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { Session } from '@supabase/supabase-js'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ session, setSession ] = useState<any>({});
+  
+
+  supabase.auth.onAuthStateChange((event, session) => {
+    console.log(event, session);
+    // if session === null DO NOT RENDER
+  })
+
+  function logOut() {
+    supabase.auth.signOut();
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }}/>
+      <button onClick={logOut}></button>
     </div>
   )
 }
---
+++ my-app/src/supabaseClient.ts
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient("https://ohjfvruvcrqreevurhdn.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oamZ2cnV2Y3JxcmVldnVyaGRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY3NDg5NjMsImV4cCI6MTk5MjMyNDk2M30.TNeBBMxc3RJiMnnalfxTwYpY2bIyxO3NFZvUWUYPTD0");
