cb9380cb919d2cefa76b720f6bd7a7b8ac8e9542
Brandon Cook <brandoncook7000@gmail.com>
2023-02-18 13:11:18 -0800

    Created base design of QuestionFill.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,33 +1,48 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import QuestionFill from './QuestionFill'
+
+type Props = {
+  question_prompt: string,
+  answers: string[],
+  answer: Number,
+
+}
+
+let temp: Props = {
+    question_prompt: "What diasdasd",
+    answers: ["hello", "dsd", "jasdsadasddfs"],
+    answer: 2,
+} as Props
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    QuestionFill(temp)
+    // <div className="App">
+    //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src="/vite.svg" className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://reactjs.org" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </div>
   )
 }
 
