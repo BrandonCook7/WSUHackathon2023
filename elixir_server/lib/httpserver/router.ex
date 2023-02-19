ff95131512d4b212fa2c44c6d71580eb3ff76de9
Brandon Cook <brandoncook7000@gmail.com>
2023-02-18 19:05:30 -0800

    Created Elixir web server foundation.

---
+++ elixir_server/lib/httpserver/router.ex
@@ -0,0 +1,19 @@
+defmodule Httpserver.Router do
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+
+  get "/" do
+    # Get request to localhost 8000
+    send_resp(conn, 200, "Welcome to my API")
+  end
+
+  get "/:name" do
+    send_resp(conn, 200, "Welcome #{name}")
+  end
+
+  match _ do # default case for requests that match nothing above
+    send_resp(conn, 404, "There is no route for you call")
+  end
+end
